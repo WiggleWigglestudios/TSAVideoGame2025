eba14b3e9aed75c4d1f87dcd9872ee4dd8f40f9c
BreckMassey <breckcmassey@gmail.com>
2024-11-27 21:31:07 -0700

    Cam
    Got some of the physics working better
    camera works now

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public GameObject[] followObjects;
+    Camera cam;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        cam = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 center=new Vector3();
+        Vector2 min = new Vector2(100000, 100000);
+        Vector2 max = new Vector2(-100000, -100000);
+        for (int i = 0; i < followObjects.Length; i++)
+        {
+            center += followObjects[i].transform.position;
+            min.x = Mathf.Min(followObjects[i].transform.position.x, min.x);
+            min.y = Mathf.Min(followObjects[i].transform.position.y, min.y);
+
+            max.x = Mathf.Max(followObjects[i].transform.position.x, max.x);
+            max.y = Mathf.Max(followObjects[i].transform.position.y, max.y);
+        }
+        center /= followObjects.Length;
+        Vector3 dir = center - transform.position;
+        dir.z = 0;
+        float distance = dir.magnitude;
+        dir.Normalize();
+
+
+        transform.position += dir*distance*Mathf.Pow(0.5f,Time.deltaTime)*Time.deltaTime;
+        cam.orthographicSize =Mathf.Max(10, Vector2.Distance(min, max)/1.5f);
+    }
+}
--- Assets/scripts/PlayerMovement.cs
+++ Assets/scripts/PlayerMovement.cs
@@ -29,29 +29,37 @@ public class PlayerMovement : MonoBehaviour
     }
     private void FixedUpdate()
     {
-        resolveTerrainCollisions();
-        playerInputs();
+
+        ////Debug.Log(getTile(transform.position));
+        int subSteps = 1;
+        for (int i = 0; i < subSteps; i++)
+        {
+            resolveTerrainCollisions();
+
+          
+            vel.y += gravity * Time.fixedDeltaTime / (float)subSteps;
+            playerInputs(subSteps); 
+            pos += vel * Time.fixedDeltaTime / (float)subSteps;
+            transform.position = new Vector3(pos.x, pos.y, -5);
+        }
 
         if (grounded)
         {
             //friction resistance
-            vel.x *= Mathf.Pow(0.1f, Time.deltaTime);
-            vel.y *= Mathf.Pow(0.1f, Time.deltaTime);
+        //   vel.x *= Mathf.Pow(0.01f, Time.deltaTime);
+        //    vel.y *= Mathf.Pow(0.01f, Time.deltaTime);
         }
-        else {
+        else
+        {
             //air resistance
-            vel.x *= Mathf.Pow(0.99f, Time.deltaTime);
-            vel.y *= Mathf.Pow(0.99f, Time.deltaTime);
+        //    vel.x *= Mathf.Pow(0.99f, Time.deltaTime);
+        //   vel.y *= Mathf.Pow(0.99f, Time.deltaTime);
         }
 
-        vel.y += gravity*Time.fixedDeltaTime;
-        pos += vel * Time.fixedDeltaTime;
-        transform.position = new Vector3(pos.x,pos.y,-5);
 
-        //Debug.Log(getFirstNumberInString("asdf12", 15));
     }
 
-    void playerInputs() 
+    void playerInputs(int subSteps) 
     {
         if (grounded)
         {
@@ -65,11 +73,11 @@ public class PlayerMovement : MonoBehaviour
 
         if (Input.GetKey(KeyCode.D))
         {
-            vel.x += speed*Time.fixedDeltaTime;
+            vel.x += speed*Time.fixedDeltaTime/ (float)subSteps;
         }
         if (Input.GetKey(KeyCode.A))
         {
-            vel.x -= speed * Time.fixedDeltaTime;
+            vel.x -= speed * Time.fixedDeltaTime / (float)subSteps;
         }
 
         if (!Input.GetKey(KeyCode.W)) 
@@ -89,28 +97,108 @@ public class PlayerMovement : MonoBehaviour
 
     void resolveTerrainCollisions() 
     {
-       Debug.Log(getTile(pos));
+        //Debug.Log("1 " + getTile(pos));
+        ////Debug.Log("2 " + getTile(new Vector2(pos.x + 1, pos.y)));
+        //Debug.Log("3 " + getTile(new Vector2(pos.x , pos.y+1)));
+        //Debug.Log("4 " + getTile(new Vector2(pos.x + 1, pos.y+1)));
         grounded = false;
-        if (pos.y < 0)
+
+       // //Debug.Log("1 " + getTile(pos));
+        if (getTile(pos)>= 1)
         {
-            pos.y = 0;
-            vel.y = 0;
-            grounded = true;
+         //   //Debug.Log("1");
+            if (Mathf.Floor(pos.x) + 1 - pos.x < Mathf.Floor(pos.y) + 1 - pos.y)
+            {
+           //     //Debug.Log("1 x");
+                pos.x = Mathf.Floor(pos.x) + 1;
+                vel.x = Mathf.Max(0,vel.x);
+            }
+            else
+            {
+              //  //Debug.Log("1 y");
+                pos.y = Mathf.Floor(pos.y) + 1;
+                vel.y = Mathf.Max(0,vel.y);
+                grounded = true;
+            }
         }
+        //Debug.Log("2 " + getTile(new Vector2(pos.x + 1, pos.y)));
+        if (getTile(new Vector2(pos.x+1,pos.y)) >= 1)
+        {
+            //Debug.Log("2");
+            if (pos.x - Mathf.Floor(pos.x) < Mathf.Floor(pos.y) + 1 - pos.y)
+            {
+                //Debug.Log("2 x");
+                pos.x = Mathf.Floor(pos.x);
+                vel.x = Mathf.Min(0, vel.x);
+            }
+            else
+            {
+                //Debug.Log("2 y");
+                pos.y = Mathf.Floor(pos.y) + 1;
+                vel.y = Mathf.Min(0, vel.y);
+                grounded = true;
+            }
+        }
+        //Debug.Log("3 " + getTile(new Vector2(pos.x, pos.y+1)));
+        if (getTile(new Vector2(pos.x, pos.y+1)) >= 1)
+        {
+            //Debug.Log("3");
+            if (Mathf.Floor(pos.x) + 1 - pos.x < pos.y - Mathf.Floor(pos.y))
+            {
+                //Debug.Log("3 x");
+                pos.x = Mathf.Floor(pos.x) + 1;
+                vel.x = Mathf.Max(0, vel.x);
+            }
+            else
+            {
+                //Debug.Log("3 y");
+                pos.y = Mathf.Floor(pos.y);
+                vel.y = Mathf.Min(0, vel.y);
+            }
+        }
+        //Debug.Log("4 " + getTile(new Vector2(pos.x + 1, pos.y+1)));
+        if (getTile(new Vector2(pos.x + 1, pos.y+1)) >= 1)
+        {
+            //Debug.Log("4");
+            if (pos.x - Mathf.Floor(pos.x) < pos.y - Mathf.Floor(pos.y))
+            {
+                //Debug.Log("4 x");
+                pos.x = Mathf.Floor(pos.x);
+                vel.x = Mathf.Min(0, vel.x);
+            }
+            else
+            {
+                //Debug.Log("4 y");
+                pos.y = Mathf.Floor(pos.y);
+                vel.y = Mathf.Min(0, vel.y);
+            }
+        }
+
     }
 
 
     int getTile(Vector2 inputPos)
     {
-        int output = 0;
-        Vector3Int gridPosition = map.WorldToCell(new Vector3(inputPos.x,inputPos.y,0));
+        Vector3Int gridPosition = map.WorldToCell(new Vector3((int)inputPos.x-1, (int)inputPos.y,0));
         TileBase tile= map.GetTile(gridPosition);
-        Debug.Log(tile.ToString());
+        ////Debug.Log(tile.ToString());
         //output=int.Parse(tile.ToString());
-
+        
+        //0 air, 1 is tile, 2 is water, 3 is lava, 4 is checkpoint
+        int[] tileNumberToType = new int[]
+        { 
+          2,1,1,1,0,
+          1,1,1,1,1,
+          1,1,1,1,1,
+          1,1,1,1,1,
+          1,1,1,1,1,
+          1,1,1,1,4,
+          0,0,3,0,3,
+          3,3,1,0
+        };         
         
 
-        return getFirstNumberInString(tile.ToString(),100);
+        return tileNumberToType[getFirstNumberInString(tile.ToString(),100)];
     }
 
 
