27e26df3e18fd0d13a27d3351b9b44f9c3a3cdf5
Jackmarris <jack.marris@govalor.com>
2025-01-17 17:17:27 -0700

    Icon Noise Integration

--- Assets/scripts/ItemUIManager.cs
+++ Assets/scripts/ItemUIManager.cs
@@ -8,10 +8,14 @@ public class ItemUIManager : MonoBehaviour
     public Player player;
     public List<GameObject> items=new List<GameObject>();
     public Sprite[] itemImages;
+    // Audio
+    public AudioSource audioSourceItem;
+    public AudioClip itemActiveNoise;
+    public AudioClip itemDeactiveNoise;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        audioSourceItem = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -33,9 +37,21 @@ public class ItemUIManager : MonoBehaviour
         items.Add(Instantiate(itemImagePrefab,transform));
         items[items.Count - 1].GetComponent<Image>().sprite= itemImages[player.items[index].itemID];
         items[items.Count - 1].GetComponent<RectTransform>().anchoredPosition3D = new Vector3(100 + (items.Count-1) * 120, -100,0);
+        // Audio
+    
+        if (audioSourceItem != null && itemActiveNoise != null)
+        {
+            audioSourceItem.PlayOneShot(itemActiveNoise);
+        }
     }
     public void removeItem( int index)
     {
+        // Audio
+        if (audioSourceItem != null && itemDeactiveNoise != null)
+        {
+            audioSourceItem.PlayOneShot(itemDeactiveNoise);
+        }
+
         Destroy(items[index]);
         items.RemoveAt(index);
     }
